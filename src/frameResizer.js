ec6acbdd82e1a18dd73a8fec70c724a8a9de3493
Jannes Höke <mail@jhoeke.de>
2020-06-02 11:29:31 +0200

    🔇 Don't log all window message events

--- src/frameResizer.js
+++ src/frameResizer.js
@@ -1,6 +1,5 @@
 export default function () {
     window.addEventListener("message", function (a) {
-        console.log(a.data);
         if (typeof data === 'string') return;
         const cmd = a.data["data-tabs-command"];
         const tgt = a.data["data-tabs-target"];
